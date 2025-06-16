3e419363e2580668e09fd33fc97797c648703545
Bat-Orgil Erdenebat <berdeneb@go.olemiss.edu>
2025-06-16 02:45:00 -0500

    Create brands.js

---
+++ src/data/brands.js
@@ -0,0 +1,17 @@
+const brands = [
+  {
+    name: "Marlboro",
+    image: "/brands/marlboro.png"
+  },
+  {
+    name: "Camel",
+    image: "/brands/camel.png"
+  },
+  {
+    name: "Newport",
+    image: "/brands/newport.png"
+  },
+  // Add more as needed
+];
+
+export default brands;
