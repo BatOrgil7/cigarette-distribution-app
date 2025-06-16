79223f3dd4a6fa4671e1be282eb1404e992f796b
Bat-Orgil Erdenebat <berdeneb@go.olemiss.edu>
2025-06-16 03:00:28 -0500

    Update brands.js

--- src/data/brands.js
+++ src/data/brands.js
@@ -2,13 +2,13 @@ const brands = [
   {
     name: "Esse Change",
     image: "/brands/Esse-Change.jpg",
-    pricePerPack: 6.5,
+    pricePerPack: 6500,
     packTypes: ["Regular", "Menthol", "100s"]
   },
   {
     name: "Marlboro",
-    image: "/brands/marlboro.png",
-    pricePerPack: 7.0,
+    image: "/brands/marlboro.jpg",
+    pricePerPack: 15,000,
     packTypes: ["Red", "Gold", "Menthol"]
   }
 ];
